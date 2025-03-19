2fbca3a8f7221d0e8b16388fc637e178f125892d
Ozodbek <ozodbekjumakulov89@gmail.com>
2025-03-19 12:22:43 +0500

    sfgfhg

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // CSS faylingiz mavjud ekanligini tekshiring
 import App from "./App"; // App.js import qilinganiga ishonch hosil qiling
+import "./index.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
